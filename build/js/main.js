353c86c22a8ce3fccbe3a5a60e36f0357d905b31
Дмитрий <kdimonr@mail.ru>
2022-09-12 13:22:02 +0700

    first commit

---
+++ build/js/main.js
@@ -0,0 +1,47 @@
+import IMask from 'imask';
+import Swiper from 'swiper';
+
+// переключатель тарифов
+window.addEventListener('load', () => {
+  const tubs = document.querySelectorAll('.sub__month-link');
+  const lists = document.querySelectorAll('.sub__list');
+
+  function changeLists(tubs, lists, jsClass, cssClass) {
+    tubs.forEach((tub, i) => {
+      tub.addEventListener('click', (evt) => {
+        evt.preventDefault();
+        tubs.forEach((el) => {
+          el.classList.remove(cssClass);
+        });
+        tub.classList.add(cssClass);
+        lists.forEach((list) => {
+          list.classList.add(jsClass);
+        });
+        lists[i].classList.remove(jsClass);
+      });
+    });
+  }
+
+  if (tubs) {
+    changeLists(tubs, lists, 'sub__list-hidden', 'sub__month-link--active');
+  }
+});
+
+// маска телефона
+
+let elements = document.querySelectorAll('.imaskjs');
+
+for (let i = 0; i < elements.length; i++) {
+  new IMask(elements[i], {
+    mask: '+{7}(000)000-00-00',
+  });
+}
+
+// слайдер отзывов
+
+const swiper = new Swiper('.swiper', {
+  navigation: {
+    nextEl: '.reviews__button-next',
+    prevEl: '.reviews__button-prev',
+  },
+});
